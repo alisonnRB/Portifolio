ff8a76656ea26b1fe41a69d1374d0d4a2709611e
alisonnRB <alisonrbjs@gmail.com>
2025-03-17 16:00:44 -0300

    feature to animate

--- src/components/dragon/castle.tsx
+++ src/components/dragon/castle.tsx
@@ -1,11 +1,58 @@
 "use client";
 
-export default function Castle() {
+import { useEffect, useRef, useState } from "react";
+import { Animations } from "@/scripts/Animations";
+
+const Castle = () => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const animation = useRef<Animations | null>(null);
+    const [isPortrait, setIsPortrait] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setIsPortrait(window.innerHeight > window.innerWidth);
+
+            const handleResize = () => {
+                setIsPortrait(window.innerHeight > window.innerWidth);
+            };
+
+            window.addEventListener("resize", handleResize);
+
+            return () => {
+                window.removeEventListener("resize", handleResize);
+            };
+        }
+    }, []);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            if (isPortrait) {
+                canvas.style.height = "50dvh";
+                canvas.style.width = "calc(50dvh * 0.390625)";
+            } else {
+                canvas.style.height = "95dvh";
+                canvas.style.width = "calc(95dvh * 0.390625)";
+            }
+        }
+    }, [isPortrait]);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            animation.current = new Animations(canvasRef.current, '/resources/castle_spriteSheet.png', 179);
+            animation.current.loop(true, 70);
+
+            return () => {
+                animation.current?.stopAnimation();
+            };
+        }
+    }, []);
+
 
     return (
-        <div className="absolute z-50 h-full w-full bg-black">
-            <div className="absolute z-60 bg-pink-500 h-full w-full animate-sprite">
-            </div>
-        </div>
+        <canvas ref={canvasRef} className="castle-canvas" style={{ height: '95dvh', width: 'calc(95dvh * 0.390625)' }}>
+        </canvas>
     );
-}
+};
+
+export default Castle;
---
+++ src/scripts/Animations.ts
@@ -0,0 +1,99 @@
+export class Animations {
+    positionX: number;
+    positionY: number;
+
+    canvas: HTMLCanvasElement;
+    spriteSheet: HTMLImageElement;
+    context: CanvasRenderingContext2D | null;
+
+    widthImage: number = 0;
+    heightImage: number = 0;
+
+    spriteNumber: number = 0;
+    widthSprite: number = 0;
+
+    currentSprite: number = 0;
+
+    offsetX: number = 0;
+    offsetY: number = 0;
+
+    animationInterval: NodeJS.Timeout | null = null;
+
+    constructor(canvasReference: HTMLCanvasElement, src: string, spriteNumber: number) {
+        this.positionX = 0;
+        this.positionY = 0;
+        this.canvas = canvasReference;
+
+        this.context = this.canvas.getContext('2d') || null;
+
+        this.spriteSheet = new Image();
+        this.spriteSheet.src = src;
+
+        this.spriteSheet.onload = () => {
+            this.widthImage = this.spriteSheet.width;
+            this.heightImage = this.spriteSheet.height;
+            this.spriteNumber = spriteNumber;
+            this.widthSprite = this.widthImage / this.spriteNumber;
+
+            this.canvasForm();
+            this.draw();
+        };
+    }
+
+    canvasForm() {
+        if (!this.canvas || !this.context) return;
+
+        const canvasWidth = this.canvas.clientWidth;
+        const canvasHeight = this.canvas.clientHeight;
+
+        this.canvas.width = canvasWidth;
+        this.canvas.height = canvasHeight;
+
+        const canvasAspect = canvasWidth / canvasHeight;
+        const spriteAspect = this.widthSprite / this.heightImage;
+
+        let newWidth = canvasWidth;
+        let newHeight = newWidth / spriteAspect;
+
+        if (newHeight < canvasHeight) {
+            newHeight = canvasHeight;
+            newWidth = newHeight * spriteAspect;
+        }
+
+        this.offsetX = (canvasWidth - newWidth) / 2;
+        this.offsetY = (canvasHeight - newHeight) / 2;
+    }
+
+    draw() {
+        if (!this.context || !this.spriteSheet.complete) return;
+
+        this.context.clearRect(0, 0, this.canvas.width || 0, this.canvas.height || 0);
+        this.context.drawImage(
+            this.spriteSheet,
+            this.widthSprite * this.currentSprite, 0, this.widthSprite, this.heightImage,
+            this.offsetX, this.offsetY, this.canvas.width || 0, this.canvas.height || 0
+        );
+    }
+
+    loop(loop: boolean, rate: number) {
+        if (this.animationInterval) {
+            clearInterval(this.animationInterval);
+        }
+
+        this.animationInterval = setInterval(() => {
+            this.currentSprite++;
+            if (loop && this.currentSprite >= this.spriteNumber) {
+                this.currentSprite = 0;
+            }
+
+            this.draw();
+        }, rate);
+    }
+
+    stopAnimation() {
+        if (this.animationInterval) {
+            clearInterval(this.animationInterval);
+            this.animationInterval = null;
+        }
+    }
+}
