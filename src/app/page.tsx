6bedd926e674967d6d6009629cfa9e7f24720700
alisonnRB <alisonrbjs@gmail.com>
2025-03-18 12:19:56 -0300

    rename to compatibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Dragon from "@/components/dragon/dragon";
+import DragonCenarie from "@/components/dragon/dragonCenarie";
 
 export default function Home() {
   return (
     <div className="h-[100dvh] w-[100dvw]">
 
-      <Dragon />
+      <DragonCenarie />
 
     </div>
   );
